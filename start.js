b45f58ef0ea97a8159146bc0db92acba79ff91ea
Mitchell Scott <mitchellscott@me.com>
2021-07-05 16:57:10 -0500

    led blinks off for 5 seconds then turns on again

--- start.js
+++ start.js
@@ -2,14 +2,21 @@ console.log("Hello Mitchell");
 
 const Gpio = require('onoff').Gpio;
 const led = new Gpio(17, 'out');
-const button = new Gpio(4, 'in', 'both');
+const button = new Gpio(18, 'in', 'rising', { debounceTimeout: 10 });
+const light = false;
+
+led.writeSync(led.readSync() ^ 1);
 
 button.watch((err, value) => {
   if (err) {
     throw err;
   }
+  led.writeSync(led.readSync() ^ 1);
+  setTimeout(_ => {
+    led.writeSync(led.readSync() ^ 1);
+  }, 5000);
 
-  led.writeSync(value);
+  console.log('this is where I take a picture');
 });
 
 process.on('SIGINT', _ => {
